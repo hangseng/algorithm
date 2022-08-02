3bf03b8865d30a66bddbde7789fe7308393b8f98
wanghengsheng <wanghengsheng2@huawei.com>
2022-08-02 17:07:16 +0800

    feat: 在排序数组中查找元素的第一个和最后一个位置

---
+++ leetcode/src/day22_802.java
@@ -0,0 +1,54 @@
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ * leetcode: https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
+ * @author: Ansen
+ * @create: 2022-08-01 11:26
+ **/
+class day22_802 {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{left_bound(nums, target), right_bound(nums, target)};
+    }
+
+    int left_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        // 搜索区间为 [left, right]
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                // 搜索区间变为 [mid+1, right]
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                // 搜索区间变为 [left, mid-1]
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 收缩右侧边界
+                right = mid - 1;
+            }
+        }
+        // 检查出界情况
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+    int right_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 这里改成收缩左侧边界即可
+                left = mid + 1;
+            }
+        }
+        // 这里改为检查 right 越界的情况，见下图
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+        return right;
+    }
+}
